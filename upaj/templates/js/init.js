bfeae40bb2412fd0488d55a9549da37fd090b743
anuj97 <anujupadhyayjnp@gmail.com>
2018-09-09 10:30:32 +0530

    templates added

---
+++ upaj/templates/js/init.js
@@ -0,0 +1,21 @@
+var slider = $('#chat-pane').slideReveal({
+    trigger: $('.trigger'),
+    width: '90%',
+    push: false,
+    position: 'right',
+    overlay: true,
+    overlayColor: 'rgba(0,0,0,0.5)',
+});
+
+// Materialize JS
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
