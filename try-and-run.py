bad3a0220e7fd9bf72df7bc449e3853d4da24ff3
anuj97 <anujupadhyayjnp@gmail.com>
2018-10-09 10:12:18 +0530

    update

--- try-and-run.py
+++ try-and-run.py
@@ -10,16 +10,24 @@ from csv import reader
 import datetime
 import time
 from sklearn import datasets,linear_model
-import tkinter as tk
-import tkinter.font as tkFont
-try:
-    import ttk as ttk
-    import ScrolledText
-except ImportError:
-    import tkinter.ttk as ttk
-    import tkinter.scrolledtext as ScrolledText
 from pprint import pprint
+import pandas as pd
+import json
 
+from sklearn.tree import DecisionTreeClassifier
+
+DATA_GOV_API ='579b464db66ec23bdd000001f4159aa056f849bb6c7922a7a5c2cc99'
+
+
+def load_csv(filename):
+    dataset = list()
+    with open(filename, 'r') as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            if not row:
+                continue
+            dataset.append(row)
+    return dataset
 
 
 conversation = watson_developer_cloud.ConversationV1(
@@ -36,12 +44,13 @@ def get_response(chat):
     response = conversation.message(workspace_id=workspace_id, input={'text': chat})
     return response
 
-def resp(chat):
+def main(chat):
 
     intents = []
     entities = []
 
-    response = get_response(chat)
+    watson_replies = get_response(chat)
+    response = watson_replies.result
     pprint(response)
 
     for intent in response['intents']:
@@ -62,18 +71,18 @@ def msg_type(intents, entities, response):
         return weather(entities)
 
     if 'crop_forecasting' in intents:
-        return crop_forecasting()
+        return crop_forecasting(entities)
 
     if 'cost' in intents:
         return cost(response)
 
     if 'pesticide' in intents:
-        return pesticide(chat)
+        return pesticide(entities)
 
     if 'goodbyes' in intents:
         return bye()
 
-def greeting(response):
+def greeting(resopnse):
 
     ''' returns greetings messages'''
 
@@ -104,7 +113,34 @@ def location_suggestions(location):
             print(location)
 
 
-def cost(response):
-    return 0
+def pesticide(entities):
 
-resp("weather jabalpur")
+    ''' returns pesticide information '''
+
+    value = entities[0]['value']
+    data = load_csv('pesticides.csv')
+    print(data)
+    pest = str(-1)
+    for i in range(1,len(data)):
+        if (data[i][0].lower() == value.lower()):
+            pest = data[i][1].lower()
+            break
+
+
+def cost(entities):
+
+    crop_data = entities[0][['value']]
+    output = msp(crop_data)
+    print(output)
+
+
+def crop_forecasting(): # pass entities in this later
+
+    data = pd.read_table('~/Documnents/Datasets/Upaj/Cauliflower_as_on_date.xml')
+    print(data.head())
+
+    clf = DecisionTreeClassifier()
+
+
+
+crop_forecasting()
